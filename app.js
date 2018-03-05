a8f9dae2fedc4629424aac1d97047cbb74ca1d9e
joshua park <joshuapark@joshuas-MacBook-Pro.local>
2018-03-05 18:18:58 -0500

    done with setting up nunjucks

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const app = express(); // creates an instance of an express application
 const morgan = require('morgan')
+const nunjucks = require('nunjucks');
 
 
 app.use(morgan('dev'))
 
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('./views/index.html', locals, (err, output) => {
+    if (err) console.error(err)
+    console.log(output);
+});
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+
 app.get('/news', (req, res, next) => {
-    res.status(200).send('hello world!')
+    res.status(200).render( 'index', {title: 'Hall of Fame', people: people} );
 })
-
-
 
 app.listen('3000', () => {
     console.log('server listening');
