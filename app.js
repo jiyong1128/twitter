168fa926938eb0e5d1298d490f6732523df9edc0
Fred Ma <fma1@terpmail.umd.edu>
2018-03-06 10:53:58 -0500

    Live Test

--- app.js
+++ app.js
@@ -2,32 +2,35 @@ const express = require('express');
 const app = express(); // creates an instance of an express application
 const morgan = require('morgan')
 const nunjucks = require('nunjucks');
+const routes = require('./routes');
 
 
 app.use(morgan('dev'))
+app.use('/', routes)
+app.use(express.static('public'))
 
-var locals = {
-    title: 'An Example',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-};
+//var locals = {
+//    title: 'An Example',
+//    people: [
+//        { name: 'Gandalf'},
+//        { name: 'Frodo' },
+//        { name: 'Hermione'}
+//    ]
+//};
 
 const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
 nunjucks.configure('views', {noCache: true});
-nunjucks.render('./views/index.html', locals, (err, output) => {
-    if (err) console.error(err)
-    console.log(output);
-});
+//nunjucks.render('index.html', locals, (err, output) => {
+//    if (err) console.error(err)
+//    console.log(output);
+//});
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 
-app.get('/news', (req, res, next) => {
-    res.status(200).render( 'index', {title: 'Hall of Fame', people: people} );
-})
+//app.get('/news', (req, res, next) => {
+//    res.status(200).render( 'index', {title: 'Hall of Fame', people: people} );
+//})
 
 app.listen('3000', () => {
     console.log('server listening');
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+// could use one line instead: const router = require('express').Router();
+const tweetBank = require('../tweetBank');
+
+router.get('/', function (req, res) {
+  let tweets = tweetBank.list();
+  console.log(tweets);
+  res.render( 'index', { tweets: tweets } );
+});
+
+//router.get('/stylesheets/style.css', function (req, res) {
+//  res.sendFile('/home/fma1/FullstackAcademy/twitter/public/stylesheets/style.css');
+//});
+
+module.exports = router;
